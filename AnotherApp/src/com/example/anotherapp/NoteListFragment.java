f42ee62e2c7f02002a380b2a787b1c7240a9bbd1
John Rapp Farnes <johnfarnes@hotmail.com>
2013-03-24 19:28:26 +0100

    Some more dialog working

--- AnotherApp/src/com/example/anotherapp/NoteListFragment.java
+++ AnotherApp/src/com/example/anotherapp/NoteListFragment.java
@@ -47,45 +47,17 @@ public class NoteListFragment extends Fragment {
 	private OnItemClickListener onItemClick = new OnItemClickListener() {
 		public void onItemClick(AdapterView<?> parent, View v, int position,
 				long id) {
-			
-			//TODO Spring clean
-			
 			Intent intent = new Intent(getActivity(), DialogActivity.class);
 			intent.putExtra(Resource.SEND_NOTE_INDEX, position);
 			intent.putExtra(Resource.SEND_LIST_INDEX, index);
 			startActivity(intent);
-			
-//			if(((MainActivity)getActivity()).actionMode == null) {
-//				Intent intent = new Intent(getActivity(), NoteDetailActivity.class);
-//				intent.putExtra(Resource.SEND_NOTE_INDEX, position);
-//				intent.putExtra(Resource.SEND_LIST_INDEX, index);
-//				startActivity(intent);
-			// } else {
-			// v.setSelected(!v.isSelected());
-			// }
 		}
 	};
-	
-//	public void loadData() {
-//		//TODO Follow this example but make it work
-//		for (Note ; i < 5; i++) {
-//			Note n = new Note("Title", "Note", "Image", new Date(10000));
-//			notes.add(n);
-//		}
-//	}
 
 	AdapterView.OnItemLongClickListener onLongClick = new AdapterView.OnItemLongClickListener() {
 		@Override
 		public boolean onItemLongClick(AdapterView<?> adapterView, View view,
 				int i, long l) {
-//			MainActivity parent = (MainActivity)getActivity();
-//			if (parent.actionMode != null) {
-//				return false;
-//			}
-//
-//			parent.actionMode = getActivity().startActionMode(parent.actionModeCallback);
-//			view.setSelected(true);
-//			return true;
 			return false;
 		}
 	};
