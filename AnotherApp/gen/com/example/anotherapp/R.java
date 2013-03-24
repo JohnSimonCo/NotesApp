189ed12258007da3d4f501b162468487b542be85
Simon Halvdansson <swesnowme@gmail.com>
2013-03-24 21:13:28 +0100

    Added orange theme

--- AnotherApp/gen/com/example/anotherapp/R.java
+++ AnotherApp/gen/com/example/anotherapp/R.java
@@ -16,6 +16,7 @@ public final class R {
     public static final class color {
         public static final int note_selected=0x7f050001;
         public static final int note_unselected=0x7f050000;
+        public static final int pressed_holoorange=0x7f050002;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -28,54 +29,83 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int card_background=0x7f020000;
-        public static final int delete=0x7f020001;
-        public static final int discard=0x7f020002;
-        public static final int done=0x7f020003;
-        public static final int ic_delete=0x7f020004;
-        public static final int ic_discard=0x7f020005;
-        public static final int ic_done=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int ic_list=0x7f020008;
-        public static final int ic_new_note=0x7f020009;
-        public static final int list=0x7f02000a;
-        public static final int new_note=0x7f02000b;
-        public static final int note=0x7f02000c;
+        public static final int ab_bottom_solid_holoorange=0x7f020000;
+        public static final int ab_solid_holoorange=0x7f020001;
+        public static final int ab_stacked_solid_holoorange=0x7f020002;
+        public static final int ab_transparent_holoorange=0x7f020003;
+        public static final int btn_cab_done_default_holoorange=0x7f020004;
+        public static final int btn_cab_done_focused_holoorange=0x7f020005;
+        public static final int btn_cab_done_holoorange=0x7f020006;
+        public static final int btn_cab_done_pressed_holoorange=0x7f020007;
+        public static final int cab_background_bottom_holoorange=0x7f020008;
+        public static final int cab_background_top_holoorange=0x7f020009;
+        public static final int card_background=0x7f02000a;
+        public static final int delete=0x7f02000b;
+        public static final int discard=0x7f02000c;
+        public static final int done=0x7f02000d;
+        public static final int ic_delete=0x7f02000e;
+        public static final int ic_discard=0x7f02000f;
+        public static final int ic_done=0x7f020010;
+        public static final int ic_launcher=0x7f020011;
+        public static final int ic_list=0x7f020012;
+        public static final int ic_new_note=0x7f020013;
+        public static final int list=0x7f020014;
+        public static final int list_focused_holoorange=0x7f020015;
+        public static final int menu_dropdown_panel_holoorange=0x7f020016;
+        public static final int new_note=0x7f020017;
+        public static final int note=0x7f020018;
+        public static final int pressed_background_holoorange=0x7f020019;
+        public static final int progress_bg_holoorange=0x7f02001a;
+        public static final int progress_horizontal_holoorange=0x7f02001b;
+        public static final int progress_primary_holoorange=0x7f02001c;
+        public static final int progress_secondary_holoorange=0x7f02001d;
+        public static final int selectable_background_holoorange=0x7f02001e;
+        public static final int spinner_ab_default_holoorange=0x7f02001f;
+        public static final int spinner_ab_disabled_holoorange=0x7f020020;
+        public static final int spinner_ab_focused_holoorange=0x7f020021;
+        public static final int spinner_ab_pressed_holoorange=0x7f020022;
+        public static final int spinner_background_ab_holoorange=0x7f020023;
+        public static final int tab_indicator_ab_holoorange=0x7f020024;
+        public static final int tab_selected_focused_holoorange=0x7f020025;
+        public static final int tab_selected_holoorange=0x7f020026;
+        public static final int tab_selected_pressed_holoorange=0x7f020027;
+        public static final int tab_unselected_focused_holoorange=0x7f020028;
+        public static final int tab_unselected_pressed_holoorange=0x7f020029;
     }
     public static final class id {
-        public static final int adapter_view=0x7f090011;
-        public static final int dialog_delete=0x7f090003;
-        public static final int dialog_edit=0x7f090004;
-        public static final int dialog_edit_note=0x7f090009;
-        public static final int dialog_edit_title=0x7f090008;
-        public static final int dialog_list_title=0x7f090005;
-        public static final int dialog_view_date=0x7f090007;
-        public static final int dialog_view_image=0x7f090006;
-        public static final int dialog_view_note=0x7f09000b;
-        public static final int dialog_view_title=0x7f09000a;
-        public static final int discard=0x7f090010;
-        public static final int done=0x7f09000f;
-        public static final int dummy=0x7f09000c;
-        public static final int edit_ui=0x7f09000e;
-        public static final int empty=0x7f090012;
-        public static final int fragment_container=0x7f090002;
-        public static final int list_item_date=0x7f090013;
-        public static final int list_item_note=0x7f090015;
-        public static final int list_item_title=0x7f090014;
-        public static final int menu_add_list=0x7f090016;
-        public static final int menu_discard=0x7f09001a;
-        public static final int menu_done=0x7f090019;
-        public static final int menu_list_spinner=0x7f09001f;
-        public static final int menu_name_list=0x7f09001b;
-        public static final int menu_new_note=0x7f090018;
-        public static final int menu_remove_list=0x7f090017;
-        public static final int menu_remove_note=0x7f090020;
-        public static final int menu_remove_notes=0x7f09001e;
-        public static final int menu_select_list=0x7f09001d;
-        public static final int menu_settings=0x7f09001c;
-        public static final int note=0x7f09000d;
-        public static final int pager=0x7f090000;
-        public static final int pager_title_strip=0x7f090001;
+        public static final int adapter_view=0x7f0a0011;
+        public static final int dialog_delete=0x7f0a0003;
+        public static final int dialog_edit=0x7f0a0004;
+        public static final int dialog_edit_note=0x7f0a0009;
+        public static final int dialog_edit_title=0x7f0a0008;
+        public static final int dialog_list_title=0x7f0a0005;
+        public static final int dialog_view_date=0x7f0a0007;
+        public static final int dialog_view_image=0x7f0a0006;
+        public static final int dialog_view_note=0x7f0a000b;
+        public static final int dialog_view_title=0x7f0a000a;
+        public static final int discard=0x7f0a0010;
+        public static final int done=0x7f0a000f;
+        public static final int dummy=0x7f0a000c;
+        public static final int edit_ui=0x7f0a000e;
+        public static final int empty=0x7f0a0012;
+        public static final int fragment_container=0x7f0a0002;
+        public static final int list_item_date=0x7f0a0013;
+        public static final int list_item_note=0x7f0a0015;
+        public static final int list_item_title=0x7f0a0014;
+        public static final int menu_add_list=0x7f0a0016;
+        public static final int menu_discard=0x7f0a001a;
+        public static final int menu_done=0x7f0a0019;
+        public static final int menu_list_spinner=0x7f0a001f;
+        public static final int menu_name_list=0x7f0a001b;
+        public static final int menu_new_note=0x7f0a0018;
+        public static final int menu_remove_list=0x7f0a0017;
+        public static final int menu_remove_note=0x7f0a0020;
+        public static final int menu_remove_notes=0x7f0a001e;
+        public static final int menu_select_list=0x7f0a001d;
+        public static final int menu_settings=0x7f0a001c;
+        public static final int note=0x7f0a000d;
+        public static final int pager=0x7f0a0000;
+        public static final int pager_title_strip=0x7f0a0001;
     }
     public static final class layout {
         public static final int actionbar_rename=0x7f030000;
@@ -92,10 +122,10 @@ public final class R {
         public static final int spinner_item=0x7f03000b;
     }
     public static final class menu {
-        public static final int activity_main=0x7f080000;
-        public static final int activity_note_dialog=0x7f080001;
-        public static final int contextual_actionbar=0x7f080002;
-        public static final int fragment_note_detail=0x7f080003;
+        public static final int activity_main=0x7f090000;
+        public static final int activity_note_dialog=0x7f090001;
+        public static final int contextual_actionbar=0x7f090002;
+        public static final int fragment_note_detail=0x7f090003;
     }
     public static final class string {
         public static final int action_settings=0x7f07000c;
@@ -116,5 +146,19 @@ public final class R {
         public static final int remove_note=0x7f07000a;
         public static final int rename_list=0x7f070009;
         public static final int title_activity_note_dialog=0x7f070011;
+    }
+    public static final class style {
+        public static final int ActionBar_Solid_Holoorange=0x7f080001;
+        public static final int ActionBar_Transparent_Holoorange=0x7f080002;
+        public static final int ActionBarTabStyle_Holoorange=0x7f080005;
+        public static final int ActionButton_CloseMode_Holoorange=0x7f080008;
+        public static final int DropDownListView_Holoorange=0x7f080004;
+        public static final int DropDownNav_Holoorange=0x7f080006;
+        public static final int PopupMenu_Holoorange=0x7f080003;
+        public static final int ProgressBar_Holoorange=0x7f080007;
+        public static final int Theme_Holoorange=0x7f080000;
+        /**  this style is only referenced in a Light.DarkActionBar based theme 
+         */
+        public static final int Theme_Holoorange_Widget=0x7f080009;
     }
 }
